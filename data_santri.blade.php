72ebd799e89a9863b7e2325862aa96b271e4628a
Hamzah146 <h4mzah.146@gmail.com>
2024-12-04 10:41:30 +0700

    Add files via upload

---
+++ data_santri.blade.php
@@ -0,0 +1,40 @@
+<style>
+table, th, td {
+  border:1px solid black;
+}
+</style>
+
+<h3>Data Santri</h3>
+
+<table style="width:30%">
+  <tr>
+    <th>Nomor</th>
+    <th>Nama</th>
+    <th>Alamat</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Fawwaz</td>
+    <td>Jakarta</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Inaya</td>
+    <td>Depok</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Zidan</td>
+    <td>Binjai</td>
+  </tr>
+  <tr>
+    <td>4</td>
+    <td>Bambang</td>
+    <td>Mojokerto</td>
+  </tr>
+  <tr>
+    <td>5</td>
+    <td>Erwin Romel</td>
+    <td>Berlin</td>
+  </tr>
+</table>
